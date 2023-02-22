04477025d3eb114c6273233ef889aeb71db36e50
kphutiyagae <kopanophutiyagae@gmail.com>
2023-02-23 01:14:41 +0200

    feat: :sparkles: Added web-api caching for Allflights API request.

--- src/scripts/api-service.js
+++ src/scripts/api-service.js
@@ -1,11 +1,26 @@
+import {
+	isRequestInCache,
+	addRequestToCache,
+	getCachedRequest,
+} from "./web-storage";
+
 const baseUrl = "http://opensky-network.org/api";
 
 async function getAllStates() {
-	return await fetch(`${baseUrl}/states/all?extended=1`)
-		.then((res) => res.json())
-		.then((data) => {
-			return data.states;
-		});
+	if (!(await isRequestInCache(`${baseUrl}/states/all?extended=1`))) {
+		addRequestToCache(`${baseUrl}/states/all?extended=1`);
+		return await getCachedRequest(`${baseUrl}/states/all?extended=1`)
+			.then((res) => res.json())
+			.then((data) => {
+				return data.states;
+			});
+	} else {
+		return getCachedRequest(`${baseUrl}/states/all?extended=1`)
+			.then((res) => res.json())
+			.then((data) => {
+				return data.states;
+			});
+	}
 }
 
 export { getAllStates };
